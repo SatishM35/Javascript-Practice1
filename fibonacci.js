6ade2ad8b248155022f66ffa3077417a6295afbb
Satish More <111186122+SatishM35@users.noreply.github.com>
2025-09-14 11:35:11 +0530

    Update Fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -1,2 +1,11 @@
-//Test 
-1212
+//Fibonacci sequence : 0 1 1 2 3 5 8 13
+// 
+function fibonacci(n) {
+    let fibSequence = [0, 1];
+    while (fibSequence.length < n) {
+        fibSequence.push(fibSequence[fibSequence.length - 1] + fibSequence[fibSequence.length - 2]);
+    }
+    return fibSequence;
+}
+
+console.log(fibonacci(10));
