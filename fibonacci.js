8c80d36e1e605357298a90aef42292decfb5da9b
Satish More <111186122+SatishM35@users.noreply.github.com>
2025-09-19 17:30:03 +0530

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -8,6 +8,15 @@ function fibonacci(n) {
     return fibSequence;
 }
 
-console.log(fibonacci(10));
+//console.log(fibonacci(10));
 
 //O/P: (10) [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+function fibonacciRecursive(n) {
+  if (n <= 1) {
+    return n;
+  }
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+}
+
+console.log(fibonacciRecursive(10))
